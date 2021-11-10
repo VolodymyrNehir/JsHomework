b10b3bc21d7659ea30598ec51006753367e6b66a
VolodymyrNehir <volodymyrnehir@gmail.com>
2021-11-11 00:09:35 +0200

    add

---
+++ lesson1/classwork/classwork.js
@@ -0,0 +1,14 @@
+{
+    let height = 23;
+    let width = 10;
+
+    let s = height * width
+    console.log(s)
+}
+{
+    let height = 10;
+    let width = 4;
+    let pi = 3.14;
+    let v = (height * width) * pi;
+    console.log(v)
+}
