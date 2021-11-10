4fb97f0080801c2f15592d8110b6af632bc6f9ab
VolodymyrNehir <volodymyrnehir@gmail.com>
2021-11-10 23:38:29 +0200

    add

---
+++ lesson1/homeWork/homeWork1.js
@@ -0,0 +1,174 @@
+//    1
+//     let a = 'hello';
+//
+//        let s = 'owu';
+//
+//        let d = 'ua';
+//
+//        let f = 'com';
+//
+//        let g = 1;
+//
+//        let h = 10;
+//
+//        let j = -999;
+//
+//        let k = 123;
+//
+//        let l = 3.14;
+//
+//        let z = 2.7;
+//
+//        let x = 16;
+//
+//        let c = true;
+//
+//        let v = false;
+//     //
+//     console.log(a);
+//     console.log(s);
+//     console.log(d);
+//     console.log(f);
+//     console.log(g);
+//     console.log(h);
+//     console.log(j);
+//     console.log(k);
+//     console.log(l);
+//     console.log(z);
+//     console.log(x);
+//     console.log(c);
+//     console.log(v);
+//     //
+//     alert(a);
+//     alert(s);
+//     alert(d);
+//     alert(f);
+//     alert(g);
+//     alert(h);
+//     alert(j);
+//     alert(k);
+//     alert(l);
+//     alert(z);
+//     alert(x);
+//     alert(c);
+//     alert(v);
+//     //
+//     document.write(a);
+//     document.write(s);
+//     document.write(d);
+//     document.write(f);
+//     document.write(g);
+//     document.write(h);
+//     document.write(j);
+//     document.write(k);
+//     document.write(l);
+//     document.write(z);
+//     document.write(x);
+//     document.write(c);
+//     document.write(v);
+// // ------- 2
+//     a = 150 + 150;
+//
+//     s = 150 + 150;
+//
+//     d = 150 + 150;
+//
+//     f = 150 + 150;
+//
+//     g = 150 + 150;
+//
+//     h = 150 + 150;
+//
+//     j = 150 + 150;
+//
+//     k = 150 + 150;
+//
+//     l = 150 + 150;
+//
+//     z = 150 + 150;
+//
+//     x = 150 + 150;
+//
+//     c = 150 + 150;
+//
+//     v = 150 + 150;
+//     //
+//     console.log(a);
+//     console.log(s);
+//     console.log(d);
+//     console.log(f);
+//     console.log(g);
+//     console.log(h);
+//     console.log(j);
+//     console.log(k);
+//     console.log(l);
+//     console.log(z);
+//     console.log(x);
+//     console.log(c);
+//     console.log(v);
+//     //
+//     alert(a);
+//     alert(s);
+//     alert(d);
+//     alert(f);
+//     alert(g);
+//     alert(h);
+//     alert(j);
+//     alert(k);
+//     alert(l);
+//     alert(z);
+//     alert(x);
+//     alert(c);
+//     alert(v);
+//     //
+//     document.write(a);
+//     document.write(s);
+//     document.write(d);
+//     document.write(f);
+//     document.write(g);
+//     document.write(h);
+//     document.write(j);
+//     document.write(k);
+//     document.write(l);
+//     document.write(z);
+//     document.write(x);
+//     document.write(c);
+//     document.write(v);
+//     //---------- 3
+//     let firstName = 'Негір';
+//     let  lastName = 'Володимир';
+//     let middName = 'Васильович';
+//
+//     const person = firstName + lastName + middName;
+//     //---------- 4
+//     let age = prompt('скільки тобі років?');
+// firstName = prompt('яка у тебе фамілія?');
+// lastName = prompt('як тебе звати?')
+// console.log(`вітаю ${lastName} ${firstName} тобі ${age} років`);
+// --------- 5
+{
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+};
+// ----------- 6
+// 5 < 6 -> true
+// 5 => 6 -> false
+// 5 == 6 -> false
+// 5 <! 6 -> false
+// 10 <= 10 -> true
+// 10 === 10 -> true
+// 10 > 10 -> false
+// 10 !== 10 -> false
+// 10 <!! 10 -> false
+// 123 === '123' -> false
+// 123 == '123' -> true
+// ---------- 7
+// document.write(str + a + "<br/>"); 205;
+// document.write(str - a + "<br/>"); 15;
+// document.write(str * "2" + "<br/>"); 40;
+// document.write(str / 2 + "<br/>"); 10;
