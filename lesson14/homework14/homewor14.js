f4706d96eb4ddd03f7b1ac8857d1406689409c3e
VolodymyrNehir <volodymyrnehir@gmail.com>
2021-12-29 23:11:57 +0200

    add

--- lesson14/homework14/homewor14.js
+++ lesson14/homework14/homewor14.js
@@ -14,8 +14,10 @@ function breakfast(haveBreakfast){
     return new Promise((resolve, reject) => {
         setTimeout(()=>{
             if (haveBreakfast)
+           haveBreakfast= 'buckwheat'
             {resolve ('cook'+' '+ haveBreakfast)}
-            reject ('i am have eat')
+            reject
+            console.log('i am have eat')
             ;
         },1000)
     })
@@ -76,76 +78,164 @@ function studying(){
         },1500)
     })
 }
-wokeUp()
-    .then(resolve=>{
-        console.log(resolve)
-        return breakfast('buckwheat')
-    })
-    .then(resolve=>{
-        console.log(resolve)
-        return shower()
-    })
-    .then(resolve=>{
-        console.log(resolve);
-        return dressed();
-    })
-    .then(resolve=>{
-        console.log(resolve);
-        return bus();
-    })
-    .then(resolve=>{
-        console.log(resolve);
-        return work();
-    })
-    .then(resolve=>{
-        console.log(resolve);
-        return comeHome();
-    })
-    .then(resolve=>{
-        console.log(resolve);
-        return supper();
-    })
-    .then(resolve=>{
-        console.log(resolve);
-        return studying();
-    })
-    .then(resolve=>{
-        console.log(resolve);
-        return sleep();
-    })
-    .then(resolve=>{
-        console.log(resolve);
-    })
-.catch(e=>{
-    console.error(e)
-});
-// async function routine(){
-//     const wokeup = await wokeUp();
-//     console.log(wokeup);
+// wokeUp()
+//     .then(resolve=>{
+//         console.log(resolve)
+//         return breakfast()
+//     })
+//     .then(resolve=>{
+//         console.log(resolve)
+//         return shower()
+//     })
+//     .then(resolve=>{
+//         console.log(resolve);
+//         return dressed();
+//     })
+//     .then(resolve=>{
+//         console.log(resolve);
+//         return bus();
+//     })
+//     .then(resolve=>{
+//         console.log(resolve);
+//         return work();
+//     })
+//     .then(resolve=>{
+//         console.log(resolve);
+//         return comeHome();
+//     })
+//     .then(resolve=>{
+//         console.log(resolve);
+//         return supper();
+//     })
+//     .then(resolve=>{
+//         console.log(resolve);
+//         return studying();
+//     })
+//     .then(resolve=>{
+//         console.log(resolve);
+//         return sleep();
+//     })
+//     .then(resolve=>{
+//         console.log(resolve);
+//     })
+// .catch(e=>{
+//     console.error(e)
+// });
+async function routine(){
+    try {
+        const wokeup = await wokeUp();
+        console.log(wokeup);
+
+        const eat = await breakfast('buckwheat');
+        console.log(eat);
+
+        const sho = await shower();
+        console.log(sho);
+
+        const newDressed = await dressed();
+        console.log(newDressed);
+
+        const newWork = await work();
+        console.log(newWork);
+
+        const newComeHome = await comeHome();
+        console.log(newComeHome);
+
+        const newSupper = await supper();
+        console.log(newSupper);
+
+        const newStudying = await studying();
+        console.log(newStudying);
+
+        const newSleep = await sleep();
+        console.log(newSleep);
+    } catch (e){
+        console.error(e);
+    }
+}
+routine();
+
+// function wokeUp(cb){
+//         setTimeout(()=>{
+//             console.log('woke up');
+//                 cb
+//             },100
+//         )
 //
-//     const eat = await breakfast('buckwheat');
-//     console.log(eat);
+// };
+// function breakfast(haveBreakfast,bc){
+//         setTimeout(()=>{
+//             if (haveBreakfast)
+//                 bc('cook'+' '+ haveBreakfast);
 //
-//     const sho = await shower();
-//     console.log(sho);
 //
-//     const newDressed = await dressed();
-//     console.log(newDressed);
+//         },1000)
+// };
+// function shower(cb){
+//         setTimeout(()=>{
+//             cb('go to the shower');
+//         },800)
 //
-//     const newWork = await work();
-//     console.log(newWork);
-//
-//     const newComeHome = await comeHome();
-//     console.log(newComeHome);
-//
-//     const newSupper = await supper();
-//     console.log(newSupper);
-//
-//     const newStudying = await studying();
-//     console.log(newStudying);
-//
-//     const newSleep = await sleep();
-//     console.log(newSleep);
-//
-// }
-// routine();
+// };
+// function dressed(cb){
+//         setTimeout(()=>{
+//             cb('dressed');
+//         },300)
+// };
+// function bus(cb){
+//         setTimeout(()=>{
+//              cb('wait for the bus');
+//         },500)
+// };
+// function work (cb){
+//         setTimeout(()=>{
+//             cb('work');
+//         },5000)
+// };
+// function comeHome(cb){
+//         setTimeout(()=>{
+//              cb('come home from work');
+//         },2000)
+// };
+// function supper(cb){
+//         setTimeout(()=>{
+//             cb('supper');
+//         },900)
+// };
+// function sleep(cb){
+//         setTimeout(()=>{
+//             cb();
+//         },10000)
+// };
+// function studying(cb){
+//         setTimeout(()=>{
+//             cb()
+//         },1500)
+// };
+// wokeUp(breakfast('buckwheat',(haveBreakfast,err)=>{
+//     console.log(haveBreakfast);
+//     shower((shower)=>{
+//         console.log(shower);
+//         dressed((dressed)=>{
+//             console.log(dressed);
+//             bus((bus)=>{
+//                 console.log(bus);
+//                 work((work)=>{
+//                     console.log(work);
+//                     comeHome((comeHome)=>{
+//                         console.log(comeHome);
+//                         supper((supper)=>{
+//                             console.log(supper);
+//                             sleep(()=>{
+//                                 console.log('sleep');
+//                                 studying(()=>{
+//                                     console.log('studying')
+//                                 })
+//                             })
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// }));
